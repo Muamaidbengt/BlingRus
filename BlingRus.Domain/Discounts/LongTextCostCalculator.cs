72732e8fb33dfe897b56b2193a330a129a3d2b01
Tomas Rosenqvist <tomas.rosenqvist@afaforsakring.se>
2018-06-18 10:43:43 +0200

    Fixade bugg i prisberäkning

--- BlingRus.Domain/Discounts/LongTextCostCalculator.cs
+++ BlingRus.Domain/Discounts/LongTextCostCalculator.cs
@@ -15,7 +15,7 @@
         {
             if (string.IsNullOrEmpty(line.Customization) || line.Customization.Length <= _textLength)
                 return;
-            line.Apply(new PriceLineAdjustment($"Text longer than {_textLength} characters", 0, _charge));
+            line.Apply(new PriceLineAdjustment($"Text longer than {_textLength} characters", 0, _charge * line.QuantityOrdered));
         }
     }
 }
