5e04edb9b454aa3a3c2605438f64be70b869e591
Tomas Rosenqvist <tomas.rosenqvist@afaforsakring.se>
2018-06-25 15:20:35 +0200

    Enterprise tweaks

--- BlingRus.Domain/EnterpriseCollections/EnterpriseBag.cs
+++ BlingRus.Domain/EnterpriseCollections/EnterpriseBag.cs
@@ -8,7 +8,7 @@ namespace BlingRus.Domain.EnterpriseCollections
         private readonly Random _enterpriseGenerator = new Random();
         protected void ActivateTheEnterprise()
         {
-            var enterpriseCoefficient = _enterpriseGenerator.Next(5, 18) / 10d;
+            var enterpriseCoefficient = _enterpriseGenerator.Next(1, 16) / 10d;
             enterpriseCoefficient *= enterpriseCoefficient;
             var enterpriseFactor = (int) Math.Pow(10, enterpriseCoefficient);
             Thread.Sleep(enterpriseFactor);
